07126d7e02fe92da145550635999918fec7db8c7
Jhonattan Saavedra Gomez <jhonattanestivensaavedragomez@gmail.com>
2023-01-29 20:29:02 -0500

    Configurando entorno

---
+++ back_end/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const port = 5000
+
+const connection = require('./mondoDB');
+
+app.get('/', (request, response) => {
+  response.end('Bienvenidos al servidor Backend corriedno....');
+})
+
+app.listen(port, function () {
+  console.log(`The server is running in the port ${port}`);
+})
---
+++ back_end/mondoDB.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/crud');
+
+const objectDB = mongoose.connection
+
+objectDB.on('connected', () => { console.log('Conexion correcta a MongoDB') });
+objectDB.on('error', () => { console.log('Error en la conexion a MongoDB') });
+
+module.exports = mongoose;
