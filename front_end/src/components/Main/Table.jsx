b423c07079f2771562ba2e3631e66903bfab91fa
Jhonattan Saavedra Gomez <jhonattanestivensaavedragomez@gmail.com>
2023-01-31 14:02:32 -0500

    Agregando estilos a la tabla pero 1er fase

--- front_end/src/components/Main/Table.jsx
+++ front_end/src/components/Main/Table.jsx
@@ -39,6 +39,68 @@ const Table = () => {
             </button>
           </div>
         </div>
+        <div className="container table">
+          <table class="table-head">
+            <thead className="head">
+              <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Title</th>
+                <th scope="col">Age</th>
+                <th scope="col">Duration</th>
+                <th scope="col">Language</th>
+                <th scope="col">Premiere</th>
+                <th scope="col">Country</th>
+                <th scope="col">Director</th>
+                <th scope="col">Edit</th>
+              </tr>
+            </thead>
+          </table>
+          <table class="table-light table-hover table-striped table-body">
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>
+                  <i className="ri-edit-line"></i>
+                  <i className="ri-delete-bin-5-line"></i>
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>
+                  <i className="ri-edit-line"></i>
+                  <i className="ri-delete-bin-5-line"></i>
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td colspan="2">Larry the Bird</td>
+                <td>@twitter</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>
+                  <i className="ri-edit-line"></i>
+                  <i className="ri-delete-bin-5-line"></i>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </main>
     </>
   );
