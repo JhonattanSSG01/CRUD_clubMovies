4170dfcbc98b1682b605940743b86a68a473e013
Jhonattan Steven Saavedra Gomez <80645321+JhonattanSSG01@users.noreply.github.com>
2023-02-08 22:13:48 -0500

    .

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import {Login} from "./components/Login/Login";
 import {DataMovie} from "./components/DataMovie/DataMovie"
 import {Sign} from './components/SignUp/Sign';
 import Header from './components/Header/Header';
@@ -12,8 +11,7 @@ function App() {
         <Routes>
           <Route path="/dataAdmin" element={<DataMovie/>}></Route>
           <Route path="/Sign" element={<Sign/>}></Route>
-          <Route path="/header" element={<Header/>}></Route>
-          <Route path="/" element={<Login/>}></Route>
+          <Route path="/" element={<Header/>}></Route>
         </Routes>
       </div>
     </BrowserRouter>
