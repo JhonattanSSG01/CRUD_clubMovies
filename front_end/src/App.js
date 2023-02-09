58c7bd3b4654048bfeb36ab4d187613b2a1d5425
Jhonattan Steven Saavedra Gomez <80645321+JhonattanSSG01@users.noreply.github.com>
2023-02-08 22:16:55 -0500

    .

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Header from './components/Header/Header';
+
+import Header from './component/Header/Header';
 
 function App() {
   return (
