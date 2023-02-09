66e23c6cb4a4ff0bbb1be37616047658f959e5ea
Jhonattan Steven Saavedra Gomez <80645321+JhonattanSSG01@users.noreply.github.com>
2023-02-08 22:18:58 -0500

    .

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-
+import {Login} from "./component/Login/Login";
+import {DataMovie} from "./component/DataMovie/DataMovie"
+import {Sign} from './component/SignUp/Sign';
 import Header from './component/Header/Header';
 
 function App() {
